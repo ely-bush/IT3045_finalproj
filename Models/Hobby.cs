f4f6e680d1f99ce6ae61bbad79cbc9421af59b27
owenoconnell1 <131276489+owenoconnell1@users.noreply.github.com>
2026-08-06 22:27:59 -0400

    Implemented Food Hobby and Movie models

--- Models/Hobby.cs
+++ Models/Hobby.cs
@@ -1,6 +1,22 @@
-﻿namespace IT3045_finalproj.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IT3045_finalproj.Models
 {
     public class Hobby
     {
+        [Key]
+        public int HobbyId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Category { get; set; }
+
+        [Required]
+        public int HoursPerWeek { get; set; }
+
+        [Required]
+        public string Description { get; set; }
     }
 }
