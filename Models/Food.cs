4240234ea6d4a5c4915b8407955cdc78d8124461
owenoconnell1 <131276489+owenoconnell1@users.noreply.github.com>
2026-08-06 22:29:57 -0400

    Implemented Food Hobby and Movie models

--- Models/Food.cs
+++ Models/Food.cs
@@ -1,6 +1,22 @@
-﻿namespace IT3045_finalproj.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IT3045_finalproj.Models
 {
     public class Food
     {
+        [Key]
+        public int FoodId { get; set; }
+
+        [Required]
+        public string FoodName { get; set; }
+
+        [Required]
+        public string MealType { get; set; }
+
+        [Required]
+        public int Calories { get; set; }
+
+        [Required]
+        public Boolean IsVegetarian { get; set; }
     }
 }
--- Models/Movie.cs
+++ Models/Movie.cs
@@ -2,5 +2,19 @@
 {
     public class Movie
     {
+        [Key]
+        public int MovieId { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Genre { get; set; }
+
+        [Required]
+        public int ReleaseYear { get; set; }
+
+        [Required]
+        public double Rating { get; set; }
     }
 }
